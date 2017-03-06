7ba8fd34c8a754686c2503bb8c6dea3bfb1ec720
Roceley Law <roceleylaw@Roceleys-MacBook.local>
2017-03-05 19:31:35 -0500

    deleted unused dir

---
+++ JiaoAn/Grade7.swift
@@ -0,0 +1,29 @@
+//
+//  Grade7.swift
+//  JiaoAn
+//
+//  Created by Marlon Ou on 2015-12-23.
+//  Copyright (c) 2015 TPTJ. All rights reserved.
+//
+
+import UIKit
+
+class Grade7: UITapGestureRecognizer{
+//comment testing
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    //tom testing
+    let name = "Grade 7"
+    
+    let problemSetArray : [Question] = [
+        Samples().samples[0],
+        Samples().samples[1]
+    ]
+
+}
